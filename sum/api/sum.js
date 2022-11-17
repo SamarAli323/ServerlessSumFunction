b15a6ae1f469d57e7f0ffd118b96a3a71532b664
SamarAli323 <58469534+SamarAli323@users.noreply.github.com>
2022-11-17 22:28:10 +0200

    Add files via upload

--- sum/api/sum.js
+++ sum/api/sum.js
@@ -4,34 +4,23 @@ module.exports.sum = async (event) => {
   if (event.body == null){
     return {
       statusCode:500,
-      body: JSON.stringify('you sent an empty body')
+      body: JSON.stringify('You sent an empty body')
     }
   }
   else{
-  const reqBody = JSON.parse(event.body)
-  if (reqBody.x == '' && reqBody.y == '' || reqBody.x == null && reqBody.y == null)
+    const reqBody = JSON.parse(event.body)
+    var typeX =typeof(reqBody.x);
+    var typeY =typeof(reqBody.y);
+    if (reqBody.x == '' || reqBody.y == '' || reqBody.x == null || reqBody.y == null || typeX != 'number' || typeY != 'number')
+      return {
+        statusCode:401,
+        body: JSON.stringify('please enter valid numbers')
+      }
+    else{
+    const Result = reqBody.x+reqBody.y
     return {
-      statusCode:401,
-      body: JSON.stringify('please enter valid numbers')
+      statusCode: 200,
+      body: JSON.stringify('The result is ' + Result)
     }
-    else if(reqBody.x == '' || reqBody.x == null)
-    return {
-      statusCode:401,
-      body: JSON.stringify('please enter valid value for x number')
-    }
-  else if(reqBody.y == '' || reqBody.y == null)
-    return {
-      statusCode:401,
-      body: JSON.stringify('please enter valid value for y number')
-    }
-  else{
-  const Result = reqBody.x+reqBody.y
-  return {
-    statusCode: 200,
-    body: JSON.stringify('The result is ' + Result)
-  }
 }};
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
